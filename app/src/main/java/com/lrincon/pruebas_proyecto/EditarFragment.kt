7c3125752561a2075a3975342f8ef0e798e990ac
Laura Rincón <81595177+LauraR287@users.noreply.github.com>
2024-05-15 15:21:03 -0500

    Update EditarFragment.kt

--- app/src/main/java/com/lrincon/pruebas_proyecto/EditarFragment.kt
+++ app/src/main/java/com/lrincon/pruebas_proyecto/EditarFragment.kt
@@ -5,9 +5,17 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
-class EditarFragment: Fragment(R.layout.fragment_ajustes_grupo)  {
+
+class EditarFragment: Fragment(R.layout.ajustes_grupo)  {
+
+    private val database = FirebaseDatabase.getInstance().reference.child("grupos")
+    private lateinit var editTextNombre: EditText
+    private lateinit var editTextDescripcion: EditText
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -15,8 +23,25 @@ class EditarFragment: Fragment(R.layout.fragment_ajustes_grupo)  {
         val buttonSiguiente = view.findViewById<Button>(R.id.siguiente)
         val buttonEditar = view.findViewById<ImageButton>(R.id.editar)
 
-        val nombreGrupo = view.findViewById<EditText>(R.id.editTextUser)
-        val DescripcionGrupo = view.findViewById<EditText>(R.id.descripcion)
+        editTextNombre = view.findViewById(R.id.nombreGrupo)
+        editTextDescripcion = view.findViewById(R.id.descripcionGrupo)
+
+        buttonSiguiente.setOnClickListener {
+            val nombre = editTextNombre.text.toString()
+            val descripcion = editTextDescripcion.text.toString()
+
+            val grupoId = database.push().key
+            if (grupoId != null) {
+                val grupo = Grupo(descripcion, grupoId, nombre)
+                database.child(grupoId).setValue(grupo)
+
+                val fragment = GrupoCreadoFragment()
+                val transaction = parentFragmentManager.beginTransaction()
+                transaction.replace(R.id.containerView, fragment)
+                transaction.addToBackStack(null)
+                transaction.commit()
+            }
+        }
 
         buttonAtras.setOnClickListener {
             val fragment = IntegrantesFragment()
@@ -24,15 +49,6 @@ class EditarFragment: Fragment(R.layout.fragment_ajustes_grupo)  {
             transaction.replace(R.id.containerView, fragment)
             transaction.addToBackStack(null)
             transaction.commit()
-        }
-
-        buttonSiguiente.setOnClickListener {
-            val fragment = GrupoCreadoFragment()
-            val transaction = parentFragmentManager.beginTransaction()
-            transaction.replace(R.id.containerView, fragment)
-            transaction.addToBackStack(null)
-            transaction.commit()
-
         }
 
         buttonEditar.setOnClickListener {
